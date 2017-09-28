0c0b11ee9e4d913eeb85199f94742b4e74b38e24
Jörg Beicht <joergbeicht@confessio-management.de>
2017-09-28 11:22:37 +0200

    add new line in the end of package file

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
   /**
-   * Jetzt mit Kommentar.
+   * @param
+   * @return
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
